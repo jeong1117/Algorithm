1137983988b8278500b6834fb565d31b59382efb
jeong1117 <130643420+jeong1117@users.noreply.github.com>
2023-12-31 18:06:26 +0900

    [D1] Title: 간단한 N 의 약수, Time: 6 ms, Memory: 13,336 KB -BaekjoonHub

---
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num = 0;
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; i++) {
+        if(num % i == 0)
+            printf("%d ", i);
+    }
+    return 0;
+}
